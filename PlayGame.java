6c2e40e656bf2be04c71579138b9efcb523f6d0e
Nicole0150 <nicole.williamson@barlcays.com>
2024-11-14 15:52:46 +0000

    started

---
+++ PlayGame.java
@@ -0,0 +1,10 @@
+public class PlayGame{
+    private int player1Score;
+    private int dealerScore;
+    
+
+    public PlayGame(){
+        this.player1Score = 0;
+        this.dealerScore = 0;
+    }
+}
