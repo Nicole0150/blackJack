c9efe45b2048bb11d084c667f166a439b6dd90a9
Nicole0150 <nicole.williamson@barlcays.com>
2024-11-14 17:38:17 +0000

    rank and suit enums done

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to my Blackjack Project");
-
+        
         //poss add in rules information?  Conditional for do you know the rules and if no, make a rules class?
-
         PlayGame game = new PlayGame();
+
     }
 }
---
+++ Suit.java
@@ -0,0 +1,16 @@
+public enum Suit{
+    SPADE("Spades"),
+    DIAMOND("Diamonds"),
+    HEART("Hearts"), 
+    CLUB("Clubs");
+    
+    String suit;
+
+    Suit(String suit){
+        this.suit = suit;
+    }
+
+    public String toString(){
+        return suit;
+    }
+}
