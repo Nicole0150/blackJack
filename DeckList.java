90f2e59a385e156571549d7c6d65bde63a4362ac
Nicole0150 <nicole.williamson@barlcays.com>
2024-11-15 13:36:46 +0000

    Can now shuffle the deck

--- DeckList.java
+++ DeckList.java
@@ -26,7 +26,17 @@ public class DeckList{
     //make a method to pull a random card from that array list out, i think it will need to involve Math.random and the remove() method as that returns an actual object from that list
     //then youd need to put in a new arraylist??
     
-
-
+    public ArrayList<Card> shuffleDeck(){
+        ArrayList<Card> shuffledDeck = new ArrayList<Card>();
+        while(deckList.size() > 0){
+            int noCardToRemove = (int)(Math.random() * (deckList.size() -1)); //had issue here because was originally using Math.random *52 which was leading to out of bounds
+            //changed to Math.random*52 -1 which was same issue but also negative numbers someimes
+            //realised needed a more dynamic variable as the length of the arraylist is changing each iteration and need to mirror that - hence using deckList.size()
+            Card removeCard = this.deckList.remove(noCardToRemove);
+            shuffledDeck.add(removeCard);
+        }
+        this.deckList = shuffledDeck;
+        return deckList;
+    }
 
 }
--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.println(card.toString());
         DeckList deck = new DeckList();
         System.out.println(deck.makeDeck().size());
+        System.out.println(deck.shuffleDeck());
         
         //poss add in rules information?  Conditional for do you know the rules and if no, make a rules class?
         PlayGame game = new PlayGame();
